3968d7faa8bcbc366b6b96e327b2cb3081fe6a88
unknown <joaovictorbronzatti@gmail.com>
2022-12-31 13:29:42 -0300

    Criado Formulario

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React from 'react';
 
 class Botao extends React.Component {
     render() {
+        const estaAtivo = true;
+        const styles = {
+            backgroundColor: estaAtivo ? "green" : "red"
+        }
         return (
-            <button>
+            <button style={styles}>
                 Botão
             </button>
         )
